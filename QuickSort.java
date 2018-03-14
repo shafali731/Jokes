3fe8ee96e14f5bd7923a0570cd4ee61633957323
james huang <jhuang20@stuy.edu>
2018-03-13 22:41:00 -0400

    it works!

--- QuickSort.java
+++ QuickSort.java
@@ -31,9 +31,9 @@ public class QuickSort
   public static void printToCSV( int[] r){
       //System.out.println("target, size (nanos), size");
       long pre = System.nanoTime();
-      qsort(r); 
+      qsort(r);
       long current = System.nanoTime();
-      System.out.println(target + ", " + r.length + ", " + (current-pre));
+      System.out.println( r.length + ", " + (current-pre));
   }
 
   //print input array
@@ -101,66 +101,16 @@ public class QuickSort
   //main method for testing
   public static void main( String[] args )
   {
-
-    //get-it-up-and-running, static test case:
-    int [] arr1 = {7,1,5,12,3};
-    System.out.println("\narr1 init'd to: " );
-    printArr(arr1);
-
-    qsort( arr1 );
-    System.out.println("arr1 after qsort: " );
-    printArr(arr1);
-
-    // randomly-generated arrays of n distinct vals
-    int[] arrN = new int[10];
-    for( int i = 0; i < arrN.length; i++ )
-    arrN[i] = i;
-
-    System.out.println("\narrN init'd to: " );
-    printArr(arrN);
-
-    shuffle(arrN);
-    System.out.println("arrN post-shuffle: " );
-    printArr(arrN);
-
-    qsort( arrN );
-    System.out.println("arrN after sort: " );
-    printArr(arrN);
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
-
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
-
-    //get-it-up-and-running, static test case w/ dupes:
-    int [] arr2 = {7,1,5,12,3,7};
-    System.out.println("\narr2 init'd to: " );
-    printArr(arr2);
-
-    qsort( arr2 );
-    System.out.println("arr2 after qsort: " );
-    printArr(arr2);
-
-
+    /*
+    int j=2000;
+  for(;j<40000; j+=500) {
     // arrays of randomly generated ints
-    int[] arrMatey = new int[20];
-    for( int i = 0; i < arrMatey.length; i++ )
-    arrMatey[i] = (int)( 48 * Math.random() );
-
-    System.out.println("\narrMatey init'd to: " );
-    printArr(arrMatey);
-
-    shuffle(arrMatey);
-    System.out.println("arrMatey post-shuffle: " );
-    printArr(arrMatey);
-
-    qsort( arrMatey );
-    System.out.println("arrMatey after sort: " );
-    printArr(arrMatey);
-    /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
-
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
+    int[] arrMatey = new int[j];
+    for( int i = 0; i < arrMatey.length; i++ ) {
+      arrMatey[i] = (int)( 10000* Math.random() );
+    }
+    printToCSV(arrMatey);
+  }*/
   }//end main
 
 }//end class QuickSort
---
+++ QuickSortTester.java
@@ -0,0 +1,26 @@
+//Team Jokes--James Huang, Shafali Gupta, Kevin Feng
+/*
+*OUR ALGORITHM:
+the goal:
+Test our QuickSort class, specifically the quicksort function, and find out the generalized runtime of our sorting algorithm
+what we did:
+We initialized a new QuickSort instance, and we added a function to the QuickSort class that allowed us to utilize the System
+timer to time the runtime of the quicksort algorithm. WE achieved this by finding the UTC from before running the function, and the UTC
+after. We then subtracted the before from the after to get the runtime in nanoseconds.  This is very similar to the printToCSV function in the
+MatrixFinder lab, and in fact is the same function call. This function outputs data in spreadsheet ready format. WE tested lengths from 2000- 40000,
+all of which are large lists
+*/
+public class QuickSortTester {
+  public static void main(String[] args) {
+    QuickSort g=new QuickSort();
+    int j=2000;
+  for(;j<40000; j+=500) {
+    // arrays of randomly generated ints
+    int[] arrMatey = new int[j];
+    for( int i = 0; i < arrMatey.length; i++ ) {
+      arrMatey[i] = (int)( j* Math.random() );
+    }
+    g.printToCSV(arrMatey);
+  }
+  }
+}
